f0bfd4c8b27ce5cb3294f24eb6539e12b8f964dc
Schye <chapichance13@gmail.com>
2019-11-18 16:23:25 +0100

    ajout d'un imput pour garder la console python ouverte

--- test_git_hub_1.py
+++ test_git_hub_1.py
@@ -1,5 +1,5 @@
-test = 0
-test1 = input("quel est votre âge ?")
-test =  test1
-print("vous avez donc ", test, " ans")
-#commentaire fait après
+test = 0 ;
+test1 = input("quel est votre âge ?") ;
+test =  test1 ;
+print("vous avez donc ", test, " ans") ;
+input("appuyez sur une touche pour continuer") ;
